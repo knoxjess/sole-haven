34a00d0a43aadfb8096aefcfc8e59ba1b3ae7015
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-05-20 12:40:09 -0500

    verifytoken/users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,29 @@
-const router = require('express').Router();
+const express = require('express');
+const { verifyTokenAndAuthorization } = require('./verifyToken');
+const User = require('../models/User'); // Import the User model
 
+const router = express.Router();
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password, process.env.PASS_SEC
+    ).toString();
+  }
 
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
